875528a07d010502eec7c8002cf07247c461cb5c
mobin zandkarimi <zandkarimimobin1234567@gmail.com>
2022-08-14 14:56:10 +0430

    second

--- in.js
+++ in.js
@@ -1,18 +1,31 @@
-const key = document.getElementById('key'),
-  code = document.getElementById('code'),
-  keyCode = document.getElementById('keyCode'),
-  keyCodeLarge = document.getElementById('keyCodeLarge'),
-  info = document.getElementById('info'),
-  hiddenElements = document.querySelectorAll('.hidden');
+// const key = document.getElementById('key'),
+//   code = document.getElementById('code'),
+//   keyCode = document.getElementById('keyCode'),
+//   keyCodeLarge = document.getElementById('keyCodeLarge'),
+//   info = document.getElementById('info'),
+//   hiddenElements = document.querySelectorAll('.hidden');
 
 
-  document.onkeydown = function (e) {
-    [].forEach.call(hiddenElements, function (el) {
-      el.classList.remove('hidden');
-    });
-    info.classList.add('hidden');
-    key.innerHTML = e.key;
-    code.innerHTML = e.code;
-    keyCode.innerHTML = e.keyCode;
-    keyCodeLarge.innerHTML = e.keyCode;
-  };
+//   document.onkeydown = function (e) {
+//     [].forEach.call(hiddenElements, function (el) {
+//       el.classList.remove('hidden');
+//     });
+//     info.classList.add('hidden');
+//     key.innerHTML = e.key;
+//     code.innerHTML = e.code;
+//     keyCode.innerHTML = e.keyCode;
+//     keyCodeLarge.innerHTML = e.keyCode;
+//   };
+
+
+let lekey = document.querySelector(".le-keys");
+let numcode = document.querySelector(".num-codes");
+let codes= document.querySelector(".codess");
+let keys = document.querySelector(".keyss");
+
+document.addEventListener("keypress", function(e){
+  lekey.innerText = `${e.key}`;
+  numcode.innerText = `${e.charCode}`;
+  codes.innerText =`${e.charCode}`;
+  keys.innerText =`${e.key}`;
+})
